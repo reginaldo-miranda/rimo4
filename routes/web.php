aa07660a031d7f35631af4d1d8560355e08b79b6
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-10-28 11:50:44 -0300

    firma menu

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes teste
 |--------------------------------------------------------------------------
+|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
@@ -11,5 +14,5 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('menu.menuPrincipal');
 });
